4ea133f65b2c5137c10000e63810c6000bfe984f
Sdm26 <118051850+Sdm26@users.noreply.github.com>
2022-11-21 22:03:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     int c = 0, d = 1, s = 0, n;
     cin >> n;
-    if(n>2) {
+    if(n>=2) {
         s = c + d;
         cout << s << endl;
         for (int i = 0; i <= (n-2); i++) {
